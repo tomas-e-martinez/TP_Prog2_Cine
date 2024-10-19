4e7d9c996697e3c9e0f2d42b0679c678578bdd0e
Tomás Martínez <tomas.emart@gmail.com>
2024-10-19 14:48:29 -0300

    primer commit, menu principal

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_CTYPE, "spanish");
+    int opcion;
+
+    while(true){
+        system("cls");
+        cout << "1." << endl;
+        cout << "0. Salir" << endl << endl;
+
+        cin >> opcion;
+
+        switch(opcion){
+        case 1:
+            break;
+        case 0:
+            return 0;
+        default:
+            cout << "Opción inválida. Intente de nuevo." << endl;
+            system("pause");
+            break;
+        }
+    }
+
+    return 0;
+}
